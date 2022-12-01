877f4c885c9a5b025e79ce10d64cfcfed9690e3c
djaumandreuonda <diego.jaumandreuonda@ada.ac.uk>
2022-12-01 23:38:13 +0000

    updated pawn (tests work)

--- src/classes/pawn.test.ts
+++ src/classes/pawn.test.ts
@@ -4,47 +4,95 @@ import {Board} from "./board";
 import {colour, type} from "./enums";
 import { Coordinate } from "./coordinate";
 describe('1. Unit test on pawn', () => {
-    describe("1.1 Black pawn located in 10", () => {
-        it('should be able to move to 20,30', () => {
-            let board = new Board();
-            let pawn = new Pawn(colour.BLACK);
-            board.boxes[1][0].setOccupied(true, pawn);
+    describe("Black pawn tests", () => {
+        describe("1.1 Black pawn located in 10", () => {
+            it('should be able to move to 20,30', () => {
+                let board = new Board();
+                let pawn = new Pawn(colour.BLACK);
+                board.boxes[1][0].setOccupied(true, pawn);
+    
+                let possibleMoves = pawn.getPossibleMoves(board);
+    
+                expect(possibleMoves[0].coordinate).toEqual(new Coordinate(2,0));
+                expect(possibleMoves[1].coordinate).toEqual(new Coordinate(3,0));
+              })
+        }), 
+        describe("1.2 Black pawn located in 31", () => {
+            it('should be able to move to 41', () => {
+                let board = new Board();
+                let pawn = new Pawn(colour.BLACK);
+                board.boxes[3][1].setOccupied(true,pawn);
+    
+                let possibleMoves = pawn.getPossibleMoves(board);
+    
+                expect(possibleMoves.length).toEqual(1);
+                expect(possibleMoves[0].coordinate).toEqual(new Coordinate(4,1));
+              })
+        }), 
+        describe("1.3 Black pawn located in 31", () => {
+            it('should be able to move to 41, and eat pawns in 40 & 42', () => {
+                let board = new Board();
+                let pawn = new Pawn(colour.BLACK);
+                let pawn1 = new Pawn(colour.WHITE); 
+                let pawn2 = new Pawn(colour.WHITE); 
+                board.boxes[3][1].setOccupied(true, pawn);
+                board.boxes[4][2].setOccupied(true, pawn1);
+                board.boxes[4][0].setOccupied(true, pawn2);
+    
+                let possibleMoves = pawn.getPossibleMoves(board);
+    
+                expect(possibleMoves.length).toBe(3);
+                expect(possibleMoves[0].coordinate).toEqual(new Coordinate(4,1));
+                expect(possibleMoves[1].coordinate).toEqual(new Coordinate(4,2));
+                expect(possibleMoves[2].coordinate).toEqual(new Coordinate(4,0));
+              })
+        }),
+        describe("1.3 Black pawn located in 10", () => {
+            it('should be able to move to 20 & 30, and eat pawns in 21', () => {
+                let board = new Board();
+                let pawn = new Pawn(colour.BLACK);
+                let pawn1 = new Pawn(colour.WHITE); 
+                board.boxes[1][0].setOccupied(true, pawn);
+                board.boxes[2][1].setOccupied(true, pawn1);
+    
+                let possibleMoves = pawn.getPossibleMoves(board);
 
-            let possibleMoves = pawn.getPossibleMoves(board);
+                expect(possibleMoves.length).toBe(3);
+                expect(possibleMoves[0].coordinate).toEqual(new Coordinate(2,0));
+                expect(possibleMoves[1].coordinate).toEqual(new Coordinate(3,0));
+                expect(possibleMoves[2].coordinate).toEqual(new Coordinate(2,1));
+              })
+        }),
+        describe("1.4 Black pawn located in 17", () => {
+            it('should be able to move to 27 & 28, and eat pawns in 26', () => {
+                let board = new Board();
+                let pawn = new Pawn(colour.BLACK);
+                let pawn1 = new Pawn(colour.WHITE); 
+                board.boxes[1][7].setOccupied(true, pawn);
+                board.boxes[2][6].setOccupied(true, pawn1);
+    
+                let possibleMoves = pawn.getPossibleMoves(board);
 
-            expect(possibleMoves[0].coordinate).toEqual(new Coordinate(2,0));
-            expect(possibleMoves[1].coordinate).toEqual(new Coordinate(3,0));
-          })
-    }), 
-    describe("1.2 Black pawn located in 31", () => {
-        it('should be able to move to 41', () => {
-            let board = new Board();
-            let pawn = new Pawn(colour.BLACK);
-            board.boxes[3][1].setOccupied(true,pawn);
-
-            let possibleMoves = pawn.getPossibleMoves(board);
-
-            expect(possibleMoves.length).toEqual(1);
-            expect(possibleMoves[0].coordinate).toEqual(new Coordinate(4,1));
-          })
-    }), 
-    describe("1.3 Black pawn located in 31", () => {
-        it('should be able to move to 41, and eat pawns in 40 & 42', () => {
-            let board = new Board();
-            let pawn = new Pawn(colour.BLACK);
-            let pawn1 = new Pawn(colour.WHITE); 
-            let pawn2 = new Pawn(colour.WHITE); 
-            board.boxes[3][1].setOccupied(true, pawn);
-            board.boxes[4][2].setOccupied(true, pawn1);
-            board.boxes[4][0].setOccupied(true, pawn2);
-
-            let possibleMoves = pawn.getPossibleMoves(board);
-
-            expect(possibleMoves.length).toBe(3);
-            expect(possibleMoves[0].coordinate).toEqual(new Coordinate(4,1));
-            expect(possibleMoves[1].coordinate).toEqual(new Coordinate(4,2));
-            expect(possibleMoves[2].coordinate).toEqual(new Coordinate(4,0));
-          })
+                expect(possibleMoves.length).toBe(3);
+                expect(possibleMoves[0].coordinate).toEqual(new Coordinate(2,7));
+                expect(possibleMoves[1].coordinate).toEqual(new Coordinate(3,7));
+                expect(possibleMoves[2].coordinate).toEqual(new Coordinate(2,6));
+              })
+        })
+    })
+    describe("White pawn tests", () => {
+        describe("1.1 White pawn located in 60", () => {
+            it('should be able to move to 40,50', () => {
+                let board = new Board();
+                let pawn = new Pawn(colour.WHITE);
+                board.boxes[6][0].setOccupied(true, pawn);
+    
+                let possibleMoves = pawn.getPossibleMoves(board);
+                console.log(possibleMoves);
+                expect(possibleMoves[0].coordinate).toEqual(new Coordinate(5,0));
+                expect(possibleMoves[1].coordinate).toEqual(new Coordinate(4,0));
+              })
+        })
     })
 
   });
--- src/classes/pawn.ts
+++ src/classes/pawn.ts
@@ -11,47 +11,42 @@ class Pawn extends Piece {
     }
     getPossibleMoves(board: Board):Box[] {
         this.possibleMoves = []; 
-        // This is just for black 
         if(!this.coordinate){
             return this.possibleMoves; 
         }
-        if(!(board.boxes[this.coordinate.x+1][this.coordinate.y].isOccupied())){
-          this.possibleMoves.push(board.boxes[this.coordinate.x+1][this.coordinate.y]);
-          if (this.coordinate.x == 1) {
-            this.possibleMoves.push(board.boxes[this.coordinate.x+2][this.coordinate.y])
-          }
-        }
-        if(board.boxes[this.coordinate.x+1][this.coordinate.y+1].isOccupied() && board.boxes[this.coordinate.x+1][this.coordinate.y+1].piece[0].colour == colour.WHITE){
-            this.possibleMoves.push(board.boxes[this.coordinate.x+1][this.coordinate.y+1])
-        } 
-        if(board.boxes[this.coordinate.x+1][this.coordinate.y-1].isOccupied() && board.boxes[this.coordinate.x+1][this.coordinate.y-1].piece[0].colour == colour.WHITE){
-            this.possibleMoves.push(board.boxes[this.coordinate.x+1][this.coordinate.y-1])
-        }
+        switch(this.colour) { 
+            case colour.BLACK: { 
+                if(!(board.boxes[this.coordinate.x+1][this.coordinate.y]?.isOccupied())){
+                    this.possibleMoves.push(board.boxes[this.coordinate.x+1][this.coordinate.y]);
+                    if (this.coordinate.x == 1) {
+                      this.possibleMoves.push(board.boxes[this.coordinate.x+2][this.coordinate.y])
+                    }
+                  }
+                  if(board.boxes[this.coordinate.x+1][this.coordinate.y+1]?.isOccupied() && board.boxes[this.coordinate.x+1][this.coordinate.y+1]?.piece[0].colour == colour.WHITE){
+                      this.possibleMoves.push(board.boxes[this.coordinate.x+1][this.coordinate.y+1])
+                  } 
+                  if(board.boxes[this.coordinate.x+1][this.coordinate.y-1]?.isOccupied() && board.boxes[this.coordinate.x+1][this.coordinate.y-1]?.piece[0].colour == colour.WHITE){
+                      this.possibleMoves.push(board.boxes[this.coordinate.x+1][this.coordinate.y-1])
+                  }
+               break; 
+            } 
+            case colour.WHITE: { 
+                if(!(board.boxes[this.coordinate.x-1][this.coordinate.y]?.isOccupied())){
+                    this.possibleMoves.push(board.boxes[this.coordinate.x-1][this.coordinate.y]);
+                    if (this.coordinate.x == 6) {
+                      this.possibleMoves.push(board.boxes[this.coordinate.x-2][this.coordinate.y])
+                    }
+                  }
+                  if(board.boxes[this.coordinate.x-1][this.coordinate.y-1]?.isOccupied() && board.boxes[this.coordinate.x-1][this.coordinate.y-1]?.piece[0].colour == colour.BLACK){
+                      this.possibleMoves.push(board.boxes[this.coordinate.x-1][this.coordinate.y-1])
+                  } 
+                  if(board.boxes[this.coordinate.x-1][this.coordinate.y+1]?.isOccupied() && board.boxes[this.coordinate.x-1][this.coordinate.y+1]?.piece[0].colour == colour.BLACK){
+                      this.possibleMoves.push(board.boxes[this.coordinate.x-1][this.coordinate.y+1])
+                  }
+               break; 
+            } 
+         }    
         return this.possibleMoves;
       }
-    // canMoveToBox(destination: Box): boolean {
-    //   if (!destination.isOccupied() && this.boxPos) {
-    //     // if the destination is not occupied and the piece is on the board
-    //     if (this.colour == colour.BLACK) {
-    //       if (
-    //         destination.coordinate.x == this.boxPos.coordinate.x &&
-    //         destination.coordinate.y < this.boxPos.coordinate.y
-    //       ) {
-    //         return true;
-    //       }
-    //       return false;
-    //     }
-    //     if (this.colour == colour.WHITE) {
-    //       if (
-    //         destination.coordinate.x == this.boxPos.coordinate.x &&
-    //         destination.coordinate.y > this.boxPos.coordinate.y
-    //       ) {
-    //         return true;
-    //       }
-    //       return false;
-    //     }
-    //   }
-    //   return false;
-    // }
   }
 export {Pawn}
