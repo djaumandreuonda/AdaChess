503550f5f40e3ee2675fc6f5c9bd86d987f0ef65
djaumandreuonda <diego.jaumandreuonda@ada.ac.uk>
2022-12-05 22:13:21 +0000

    Cleaning code, first MVP finished

--- my-app/src/app/game/game.component.ts
+++ my-app/src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Board } from './board/model/board.model';
 import { Coordinate } from './board/model/coordinate.model';
 import { state } from '../shared/enums/state.enum';
@@ -9,7 +9,7 @@ import { colour } from '../shared/enums/colour.enum';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
-export class GameComponent implements OnInit {
+export class GameComponent {
   board:Board; 
   turn:colour;
   state:state;
@@ -22,8 +22,6 @@ export class GameComponent implements OnInit {
     this.turn = colour.WHITE;
     this.state = state.AWAIT;
   }
-  ngOnInit():any{
-  }
   selectPiece(coordinate:Coordinate):boolean{
     if(this.board.boxes[coordinate.x][coordinate.y].piece?.colour == this.turn){ // if the player is clicking on one of their pieces 
       return true; // assume player is trying to select a piece
@@ -34,10 +32,8 @@ export class GameComponent implements OnInit {
     return false; 
   }
   registerCoordinate(coordinate:Coordinate){
-
     if(this.state == state.ATTEMPTMOVE){ // if the player is trying to move the piece 
       let validMove = this.isValidMove(coordinate);
-      console.log(validMove);
       if(!validMove){ // if is not a valid movement, change the state back
         this.possibleMoves = []; 
         this.state = state.AWAIT; // cancel the attempt to move
@@ -50,18 +46,25 @@ export class GameComponent implements OnInit {
     if(this.state == state.AWAIT){ // if player hasn't clicked on piece
       if(this.selectPiece(coordinate)){ // check it is trying to click a piece 
         this.state = state.ATTEMPTMOVE; // change status, player is trying to move a piece
+        this.prevCoordinate = coordinate; // once status has been changed, the previous move needs to be reference to know what piece the player is trying to move
         switch(this.board.boxes[coordinate.x][coordinate.y].piece.type) {
           case 'p':
+            console.log("Storing possible movements for pawn");
             break;
           case 'b':
+            console.log("Storing possible movements for bishop");
             break;
           case 'r':
+            console.log("Storing possible movements for rook");
             break;
           case 'k':
-            break;
-          case 'K':
+            console.log("Storing possible movements for knight");
             break;
           case 'q':
+            console.log("Storing possible movements for queen");
+            break;
+          case 'K':
+            console.log("Storing possible movements for king");
             break;
         }
       }
